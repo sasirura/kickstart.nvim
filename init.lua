fdf2bb8f65e9deb7717a80742cdac032b627551e
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2024-05-19 15:32:14 +0530

    added oil and typescript plugins

--- init.lua
+++ init.lua
@@ -889,6 +889,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.typescript-tools',
+  require 'kickstart.plugins.oil',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
