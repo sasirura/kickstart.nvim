6c17ba66fdcb1830d441b5c4f79a3bce725e09b6
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2025-07-21 17:01:55 +0530

    remove vim.diog line comment

--- init.lua
+++ init.lua
@@ -178,14 +178,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-vim.diagnostic.config {
-  virtual_text = true, -- shows inline messages
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
-}
-
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
